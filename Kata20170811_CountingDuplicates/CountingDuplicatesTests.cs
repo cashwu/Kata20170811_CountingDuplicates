ee3a4db52bf77d9be647f93219488f374764c2c9
cash <cash@cashwu.com>
2017-08-11 11:54:23 +0800

    add abca test case

--- Kata20170811_CountingDuplicates/CountingDuplicatesTests.cs
+++ Kata20170811_CountingDuplicates/CountingDuplicatesTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170811_CountingDuplicates
@@ -13,13 +15,32 @@ namespace Kata20170811_CountingDuplicates
             var actual = kata.DuplicateCount("abc");
             Assert.AreEqual(0, actual);
         }
+
+        [TestMethod]
+        public void input_abca_should_return_1()
+        {
+            var kata = new Kata();
+            var actual = kata.DuplicateCount("abca");
+            Assert.AreEqual(1, actual);
+        }
     }
 
     public class Kata
     {
         public int DuplicateCount(string str)
         {
-            return 0;
+            var result = 0;
+            var oldChar = new List<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.Count(a => a == str[i]) > 1 
+                    && !oldChar.Contains(str[i]))
+                {
+                    oldChar.Add(str[i]);
+                    result++;
+                }
+            }
+            return result;
         }
     }
 }
